ac4931c3f4ce83de74917972886c4f7254387de9
Benjie Gillam <benjie@jemjie.com>
2017-06-15 15:51:06 +0100

    Actually building GraphQL schema

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const pg = require("pg");
 const fs = require("fs");
 const { promisify } = require("util");
+const camelcase = require("lodash/camelcase");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 
 const readFile = promisify(fs.readFile);
 
@@ -40,14 +42,138 @@ const withPgClient = async (pgConfig = process.env.DATABASE_URL, fn) => {
   return result;
 };
 
-const typesFromPg = async pgConfig => {
+const defaultInflection = {
+  table: camelcase,
+  column: camelcase,
+  singleByKeys: (typeName, keys) =>
+    camelcase(`${typeName}-by-${keys.join("-and-")}`), // postsByAuthorId
+};
+
+const QueryPlugin = listener => {
+  listener.on("schema", async (spec, { process, extend }) => {
+    const queryType = await process(GraphQLObjectType, {
+      name: "Query",
+      fields: {},
+    });
+    return extend(spec, {
+      query: queryType,
+    });
+  });
+};
+
+const RowByPrimaryKeyPlugin = listener => {
+  listener.on(
+    "objectType:fields",
+    (spec, { process, extend }, { spec: { name } }) => {
+      if (name !== "Query") {
+        return;
+      }
+      return extend(spec, {
+        hello: {
+          type: GraphQLString,
+          resolve() {
+            return "World";
+          },
+        },
+      });
+    }
+  );
+};
+
+const ColumnsPlugin = listener => {
+  listener.on("fields", ({ name }) => name === "Query", typeSpec => {});
+};
+
+const defaultPlugins = [QueryPlugin, RowByPrimaryKeyPlugin, ColumnsPlugin];
+
+const schemaFromPg = async (
+  pgConfig,
+  { schema, inflection = defaultInflection, plugins = defaultPlugins }
+) => {
+  if (!Array.isArray(schema)) {
+    throw new Error("Argument 'schema' (array) is required");
+  }
   return withPgClient(pgConfig, async pgClient => {
     // Perform introspection
     const introspectionQuery = await readFile(INTROSPECTION_PATH, "utf8");
-    const { rows } = await pgClient.query(introspectionQuery);
-    return rows;
+    const { rows } = await pgClient.query(introspectionQuery, [schema]);
+
+    const introspectionResultsByKind = rows.reduce(
+      (memo, { object }) => {
+        memo[object.kind].push(object);
+        return memo;
+      },
+      {
+        namespace: [],
+        class: [],
+        attribute: [],
+        type: [],
+        constraint: [],
+        procedure: [],
+      }
+    );
+
+    const context = {
+      introspectionResultsByKind,
+      extend(obj, obj2) {
+        const keysA = Object.keys(obj);
+        const keysB = Object.keys(obj2);
+        for (const key of keysB) {
+          if (keysA.includes(key)) {
+            throw new Error(`Overwriting key '${key}' is not allowed!`);
+          }
+        }
+        return Object.assign({}, obj, obj2);
+      },
+      async process(Type, spec) {
+        let newSpec = spec;
+        if (Type === GraphQLSchema) {
+          newSpec = await listener.applyHooks("schema", newSpec, {
+            spec: newSpec,
+          });
+        } else if (Type === GraphQLObjectType) {
+          newSpec = await listener.applyHooks("objectType", newSpec, {
+            spec: newSpec,
+          });
+          newSpec = Object.assign({}, newSpec, {
+            fields: await listener.applyHooks(
+              "objectType:fields",
+              newSpec.fields,
+              {
+                spec: newSpec,
+                fields: newSpec.fields,
+              }
+            ),
+          });
+        }
+        return new Type(newSpec);
+      },
+    };
+
+    const listener = {
+      hooks: [],
+      on(event, fn) {
+        this.hooks[event] = this.hooks[event] || [];
+        this.hooks[event].push(fn);
+      },
+      async applyHooks(event, spec, position) {
+        let newSpec = spec;
+        const hooks = this.hooks[event] || [];
+        for (const hook of hooks) {
+          const result = await hook(newSpec, context, position);
+          if (result) {
+            newSpec = result;
+          }
+        }
+        return newSpec;
+      },
+    };
+    for (const plugin of plugins) {
+      plugin(listener);
+    }
+    return context.process(GraphQLSchema, {});
   });
 };
 
 exports.withPgClient = withPgClient;
-exports.typesFromPg = typesFromPg;
+exports.schemaFromPg = schemaFromPg;
--- test.js
+++ test.js
@@ -1,6 +1,20 @@
-const { typesFromPg } = require(".");
+const { schemaFromPg } = require(".");
+const fs = require("fs");
+const path = require("path");
+const { graphql } = require("graphql");
+const { introspectionQuery, printSchema } = require("graphql/utilities");
+
 (async () => {
-  console.dir(await typesFromPg());
+  const schema = await schemaFromPg(process.env.DATABASE_URL, {
+    schema: ["hookhaven_data"],
+  });
+  console.log(printSchema(schema));
+  const result = await graphql(schema, introspectionQuery);
+  if (result.errors) {
+    console.error("ERROR introspecting schema: ", result.errors);
+    process.exit(1);
+  }
+  process.exit(0);
 })().catch(e => {
   console.error("ERROR!");
   console.dir(e);
